7307062ea71fe9ca81f981aab8dcf401906cecb4
Raj Ramcharan <rrramcha@avans.nl>
2020-05-08 12:44:13 +0200

    added navbar

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -2,17 +2,6 @@
 
 @section('content')
 <div class="row">
-        <div class="col-1">
-            <p>1</p>
-        </div>
-    <div class="col-1">
-        <h1>Hello</h1>
-    </div>
-    <div class="col-3">
-        <h1>Hello</h1>
-    </div>
-    <div class="col-3">
-        <h1>Hello</h1>
-    </div>
+
 </div>
 @endsection
---
+++ resources/views/layout/nav.blade.php
@@ -0,0 +1,29 @@
+<section class="header">
+    <div class="container">
+        <nav class="navbar navbar-expand-xl navbar-light">
+            <a class="navbar-brand navbar-brand-main text-center" href="/">
+                <img src="{{asset('storage/pictures/dragon-small.png')}}" height="75" alt="logo">
+            </a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown"
+                    aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse justify-content-xl-around" id="navbarNavDropdown">
+                <ul class="nav navbar-nav ">
+                    <li class="nav-item ">
+                        <a class="nav-link" href="#">Menu</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link"  href="#">Nieuws</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Contact</a>
+                    </li>
+                </ul>
+            </div>
+            <a class="navbar-brand navbar-brand-main p-md-0 p-0 text-center" href="/">
+                <img src="{{asset('storage/pictures/dragon-small-flipped.png')}}" height="75" alt="logo">
+            </a>
+        </nav>
+    </div>
+</section>
